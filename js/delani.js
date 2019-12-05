414776d3ccfb3db8a1d1fb4727b87ba32048e181
NYAGO-MUREITHI <murraygoogz@gmail.com>
2019-12-05 11:13:40 +0300

    add function to toggle between hide and show design icon on hover in and out

--- js/delani.js
+++ js/delani.js
@@ -1,1 +1,8 @@
 $(document).ready(function(){
+  $(".imagedes").hover(function(event){
+    $(".imagedes").hide();
+    $("#design").show();
+  }, function(){
+    $(".imagedes").show();
+    $("#design").hide();
+  });
